4c325d9e1c774b97b7f5fa8af0c428b7d1096170
John Walker <jewalker7842@gmail.com>
2023-04-18 09:30:47 -0500

    initial car tracker

---
+++ car_pedestrian_tracker.py
@@ -0,0 +1,45 @@
+import cv2
+
+# read the video
+video_file = "your_video.mp4"
+video = cv2.VideoCapture(video_file)
+pedestrian_tracker_file = "pedestrian_tracker.xml"
+car_tracker_file = "car_tracker.xml"
+
+# import pedestrian and car tracker
+pedestrian_tracker = cv2.CascadeClassifier(pedestrian_tracker_file)
+car_tracker = cv2.CascadeClassifier(car_tracker_file)
+
+# eternally loop until q is pressed
+while True:
+
+    # vidoes are collection of images so we need to read this video as it comes in
+    successful_video_read, frame = video.read()
+
+    # if read is successful then convert video to greyscale
+    if successful_video_read:
+        grayscaled_image = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    else:
+        break
+
+    # get the coordinates for pedestrian and cars
+    pedestrian_coordinates = pedestrian_tracker.detectMultiScale(grayscaled_image)
+    car_coordinates = car_tracker.detectMultiScale(grayscaled_image)
+
+    # draw the square on the greyscale image for pedestrian and car
+    for (x, y, w, h) in car_coordinates:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+    for (x, y, w, h) in pedestrian_coordinates:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+
+    # display the video feed in a window
+    cv2.imshow(f"{video}", frame)
+    key = cv2.waitKey(1)
+
+    # exit program if the q key is pressed
+    if key == 1 or key == 113:
+        break
+
+# release the wevcam resource
+video.release()
